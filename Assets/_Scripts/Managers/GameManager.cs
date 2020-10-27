7e36b1545c5be205afaf129b7f5e1628687b85da
ToprakOzgur <zgrtprk@gmail.com>
2020-10-27 13:52:27 +0300

    game states added

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-  // Start is called before the first frame update
-  void Start()
+  private BaseState currentState;
+  public BaseState CurrentState
   {
-
+    get { return currentState; }
   }
 
-  // Update is called once per frame
+  //Changes the current game state
+  public void SetState(System.Type newStateType)
+  {
+    if (currentState != null)
+    {
+      currentState.OnDeactivate();
+    }
+
+    currentState = GetComponentInChildren(newStateType) as BaseState;
+
+    if (currentState != null)
+    {
+      currentState.OnActivate();
+    }
+  }
+
+  private void Start()
+  {
+    SetState(typeof(MenuState));
+  }
   void Update()
   {
-
+    if (currentState != null)
+    {
+      currentState.OnUpdate();
+    }
   }
 }
