32975440002306a446d5b65cf7b2856474eb4c8e
ToprakOzgur <zgrtprk@gmail.com>
2020-10-28 09:01:14 +0300

    sling animation added

---
+++ Assets/_Scripts/Sling.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sling : MonoBehaviour
+{
+  #region Edges
+
+  [System.Serializable]
+  public struct Edges
+  {
+    public Transform left;
+    public Transform right;
+
+  }
+  public Edges edgeTransforms;
+
+  #endregion
+
+  #region properties
+  private float maxBindAngle = 30;
+  private int bindSensibility = 30;
+  private float bounceOutAnimDuration = 0.6f;
+
+  #endregion
+
+
+
+  #region functions
+
+  //Rotation of Sling when targeting
+  public void Bend(float endDragPos, float startDragPos)
+  {
+    var dragMagnitude = startDragPos - endDragPos;
+
+    if (dragMagnitude < 0) return;
+
+    var bindMagnitude = dragMagnitude * bindSensibility;
+
+    //clamping max value to maxBindAngle
+    var angleClamped = bindMagnitude > maxBindAngle ? maxBindAngle : bindMagnitude;
+
+    transform.eulerAngles = new Vector3(-angleClamped, transform.eulerAngles.y, transform.eulerAngles.z);
+  }
+
+  public void VibrationAnimToOriginalPosAfterLaunch()
+  {
+    StartCoroutine(Move());
+  }
+  IEnumerator Move()
+  {
+    Vector3 startPosition = transform.position;
+    Quaternion startRotation = transform.rotation;
+
+    var t = 0f;
+    while (t < 1)
+    {
+      t += Time.deltaTime / bounceOutAnimDuration;
+      transform.rotation = Quaternion.Lerp(startRotation, Quaternion.identity, Ease.BounceEaseOut(t));
+      yield return null;
+    }
+  }
+
+
+  #endregion
+}
+
+
+
--- Assets/_Scripts/ThrowObject.cs
+++ Assets/_Scripts/ThrowObject.cs
@@ -6,33 +6,59 @@ public class ThrowBase : MonoBehaviour
 {
   protected Vector3 mOffset;
   protected float mZCoord;
+  protected Sling sling;
+  protected Vector3 touchStartPoint;
 
+
+  #region Unity functions
+
+  protected void Awake()
+  {
+    sling = transform.parent.gameObject.GetComponent<Sling>();
+  }
+
+  #endregion
+
+  #region Drag functions
   protected void OnMouseDown()
   {
-    Debug.Log("mouse down");
-
-
     mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
 
-    mOffset = gameObject.transform.position - GetMouseWorldPos();
+    touchStartPoint = GetMouseWorldPos();
+    mOffset = gameObject.transform.position - touchStartPoint;
   }
 
   protected void OnMouseUp()
   {
-    Debug.Log("mouse up");
+    touchStartPoint = Vector3.zero;
+    sling.VibrationAnimToOriginalPosAfterLaunch();
   }
-
-
-
   protected Vector3 GetMouseWorldPos()
   {
-    Vector3 mousePoint = Input.mousePosition;
+    var mousePoint = Input.mousePosition;
     mousePoint.z = mZCoord;
+
     return Camera.main.ScreenToWorldPoint(mousePoint);
   }
   protected void OnMouseDrag()
   {
-    transform.position = GetMouseWorldPos() + mOffset;
+
+    var worldPos = GetMouseWorldPos();
+
+    //for ball movement left-right
+    var newPos = worldPos + mOffset;
+    newPos.x = Mathf.Clamp(newPos.x, sling.edgeTransforms.left.position.x, sling.edgeTransforms.right.position.x);
+
+    //for sling angle
+    sling.Bend(worldPos.y, touchStartPoint.y);
+
+    transform.position = new Vector3(newPos.x, transform.position.y, transform.position.z);
+
+
 
   }
+
+
+
+  #endregion
 }
