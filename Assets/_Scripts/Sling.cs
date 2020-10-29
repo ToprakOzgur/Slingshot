6839f9f9e1d91600b6152d9508ffa52613594640
ToprakOzgur <zgrtprk@gmail.com>
2020-10-29 12:34:00 +0300

    targeting line added

--- Assets/_Scripts/Sling.cs
+++ Assets/_Scripts/Sling.cs
@@ -16,6 +16,15 @@ public class Sling : MonoBehaviour
 
   [Header("Reference to Gameobjects")]
   [SerializeField] private ThrowObjectContainer objectContainer;
+
+
+  [Header("Line Path")]
+  [SerializeField] private LineRenderer lineVisual;
+  [SerializeField] private int lineSegment = 10;
+  [SerializeField] private Transform targetingPoint;
+  private ThrowObject throwItem;
+  [SerializeField] private int throwPower = 20;
+
   #endregion
 
   #region Unity functions
@@ -24,31 +33,43 @@ public class Sling : MonoBehaviour
   private void OnEnable()
   {
     ThrowObject.OnLaunched += Launch;
-    ThrowObject.OnDrag += Rotate;
+    ThrowObject.OnDrag += RotateAndShowPath;
   }
 
   //unregister from throwable object events
   private void OnDisable()
   {
     ThrowObject.OnLaunched -= Launch;
-    ThrowObject.OnDrag -= Rotate;
+    ThrowObject.OnDrag -= RotateAndShowPath;
   }
-
+  private void Start()
+  {
+    lineVisual.positionCount = lineSegment;
+    throwItem = objectContainer.GetTopObject();
+  }
   #endregion
 
   #region functions
 
   private void Launch()
   {
-    var throwItem = objectContainer.GetTopObject();
     throwItem.gameObject.transform.parent = null;
     throwItem.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-    throwItem.gameObject.GetComponent<Rigidbody>().velocity = transform.forward * 20;
+    throwItem.gameObject.GetComponent<Rigidbody>().velocity = throwItem.gameObject.transform.transform.forward * throwPower;
     VibrationAnimToOriginalPosAfterLaunch();
     objectContainer.PrepareTopObject();
+    throwItem = null;
+    lineVisual.enabled = false;
   }
 
   //Rotation of Sling while targeting
+  private void RotateAndShowPath(Vector3 endDragPos, Vector3 startDragPos)
+  {
+
+    Rotate(endDragPos, startDragPos);
+    DrawPath(endDragPos, startDragPos);
+  }
+
   private void Rotate(Vector3 endDragPos, Vector3 startDragPos)
   {
 
@@ -66,6 +87,13 @@ public class Sling : MonoBehaviour
     transform.eulerAngles = new Vector3(-verticalRotationClamped, horizontalRotationClamped, transform.eulerAngles.z);
   }
 
+  private void DrawPath(Vector3 endDragPos, Vector3 startDragPos)
+  {
+    if (throwItem == null)
+      throwItem = objectContainer.GetTopObject();
+    lineVisual.enabled = true;
+    VisualizePath(throwItem.transform.forward * throwPower);
+  }
   private void VibrationAnimToOriginalPosAfterLaunch()
   {
     StartCoroutine(Move());
@@ -84,6 +112,28 @@ public class Sling : MonoBehaviour
     }
   }
 
+  private void VisualizePath(Vector3 vo)
+  {
+    for (int i = 0; i < lineSegment; i++)
+    {
+      Vector3 pos = CalculatePositionInTime(vo, i / (float)lineSegment);
+      lineVisual.SetPosition(i, pos);
+    }
+  }
+  private Vector3 CalculatePositionInTime(Vector3 vo, float time)
+  {
+    Vector3 Vxz = vo;
+    Vxz.y = 0f;
+
+    Vector3 result = throwItem.gameObject.transform.position + vo * time;
+    //y pos
+    float sY = (-0.5f * Mathf.Abs(Physics.gravity.y) * (time * time)) + (vo.y * time) + throwItem.gameObject.transform.position.y;
+
+    result.y = sY;
+
+    return result;
+
+  }
   #endregion
 }
 
